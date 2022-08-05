b996a76867811c3b2026172ce0e406f025c3e64a
BeengBeeng <110025541+BeengBeeng@users.noreply.github.com>
2022-08-05 11:34:47 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+// bài 1
+// let str = prompt("Mời người dùng nhập : ");
+// let a = "";
+// for (let i = str.length - 1; i >= 0; i = i - 1){
+//     a = a + str[i]
+// } console.log(a);
+
+// bài 2 
+// let str = prompt("Mời người dùng nhập : ");
+// let str2 = str.split(" ");
+// for (let i =0; i<=str2.length-1;i=i+1){
+//     str2 = str2[i][0].toUpperCase() + str2.splice[1];
+// }
+// console.log(str2);
+
+
+// bài 3
+// let str = prompt("Mời người dùng nhập : ");
+// let outPut = []
+// let test = -1;
+// for (i = 0; i <= str.length - 1; i++){
+//     if (test.indexOf(arr[i])=== -1){
+//         outPut.splice[i,0]
+//     }
+// }
+
+
+// bài 5
+// let academy = ["Cường bự", "Phú còi", "Dũng nobita"];
+// let user = prompt("Mời anh hùng bàn phím dùng nhập R/C/U/D/");
+// if (user === "R") {
+//     for (i = 0; i <= academy.length - 1; i++) {
+//     } 
+// } else if (user === "C") {
+//     let newTvien = prompt("Mời thánh dùng nhập: ");
+//     academy.push(newTvien);
+//     for (i = 0; i <= academy.length - 1; i++) {
+//         console.log(`${i + 1}. ${academy[i]}`)
+//     }
+// } else if (user === "U") {
+//     user1 = prompt("Mời thánh nhập vị trí cần update : ");
+//     user2 = prompt(" Mời nhập tên thành viên cần updete : ");
+//     academy[user1 - 1] = user2;
+// } else if (user === "D") {
+//     user3 = prompt("Mời thánh nhập vị trí cần sa thải : ");
+//     academy.splice(user3 - 1,1)
+// }
+// console.log(academy)
+
+
+// bài 6
+// let ngay = prompt("Mời người dùng nhập ngày : ");
+// let thang = prompt("Mời người dùng nhập tháng : ");
+// let nam = prompt("Mời người dùng nhập năm : ");
+// let check = [ngay, thang , nam];
+// for (i = 0; i <= check.length - 1; i++){
+//     if (check[i] > 0 && check[i]%1===0){
+//     }
+// }
+// console.log("true");
